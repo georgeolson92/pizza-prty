f7a1b7c8500882dab9d46dbc140f9e471bf802ea
George Olson <georgeolson@Georges-MacBook-Air.local>
2016-06-17 09:51:08 -0700

    Set up object functionality in scripts.js that creates object from input values and a prototype that calculates the order price, and display results in receipt section on bottom of the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,64 @@
 
+// Create order object
+function Order(size, toppings){
+  this.size = size;
+  this.toppings = toppings;
+}
 
+// Declare global variables
+var pizzaToppings = [];
+var price = 0;
 
-
-
+// Calculate price of order
+Order.prototype.priceCalculate = function() {
+  var orderPrice = 0;
+  if (this.size === "Large"){
+    var orderPrice = 16;
+    console.log("large price: " + orderPrice);
+  } else {
+    var orderPrice = 12;
+    console.log("reg price: " + orderPrice);
+  }
+  console.log("Outside of conditional: " + orderPrice);
+  for (var index = 0; index < pizzaToppings.length; index += 1) {
+    var orderPrice = orderPrice + 1;
+  }
+  this.price = orderPrice;
+  return orderPrice;
+}
 
 
 $(document).ready(function(){
-
-
   $("form#pizza-order").submit(function(event){
+    debugger;
     event.preventDefault();
-    var pizzaSize = $(".radio-size input").val();
-    var pizzaToppings = [];
+
+    // collect info from user and create new object
+    var pizzaSize = $(".radio-size input[type='radio']:checked").val();
+    console.log(pizzaSize);
+    console.log(pizzaToppings);
+    var neworder = new Order(pizzaSize, pizzaToppings);
+    console.log("Size of pizza: " + neworder.size);
+
+    // loop through toppings and add to array
     $.each($("input[name='topping']:checked"), function(){
       pizzaToppings.push($(this).val());
     });
 
-    console.log(pizzaSize);
-    console.log(pizzaToppings);
+    // call on price calculation
+    var orderPrice = (neworder.priceCalculate());
+    console.log(neworder.price);
+    console.log(neworder);
+    console.log(neworder.size, neworder.toppings);
+
+    // display results in receipt section
+    $("#pizza-size-result").text(pizzaSize);
+    for (var index = 0; index < pizzaToppings.length; index += 1) {
+      $("#toppings-result").append("<li>" + pizzaToppings[index] + "</li>");
+    }
+    $(".price").text("$" + neworder.price);
+    $("#receipt").slideDown();
+
   });
-
-
-
-
 
 });
--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@ create object from input values (pizza size, toppings array)
  input: small, pepperoni, artichokes
  output: Pizza {small, [pepperoni, artichokes]}
 
+ create prototype to calculate price
+
 display object in receipt below form
  input: small, pepperoni, artichokes
  output: small pizza with the following topping(s): pepperoni, artichokes
